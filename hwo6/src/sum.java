3f156fbc3471176db31778772503162dcecfc5e5
Maxiemusnguyen <142671963+Maxiemusnguyen@users.noreply.github.com>
2023-10-28 18:38:46 -0700

    Update sum.java

--- hwo6/src/sum.java
+++ hwo6/src/sum.java
@@ -14,15 +14,15 @@ public class sum {
     public static void main(String[] args) {
         sum calculator = new sum();
 
-        // Calling the sum(int, int) method
+        
         int intSum = calculator.sum(5, 7);
         System.out.println("Sum of integers: " + intSum);
 
-        // Calling the sum(double, double) method
+        
         double doubleSum = calculator.sum(3.5, 2.0);
         System.out.println("Sum of doubles: " + doubleSum);
 
-        // Calling the sum(double, int) method
+      
         double mixedSum = calculator.sum(4.0, 3);
         System.out.println("Sum of double and int: " + mixedSum);
     }
