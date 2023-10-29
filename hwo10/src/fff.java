ba26d120089aa4444dbdf0246e90ebbea629b6e7
Maxiemusnguyen <142671963+Maxiemusnguyen@users.noreply.github.com>
2023-10-28 17:27:15 -0700

    Add files via upload

---
+++ hwo10/src/fff.java
@@ -0,0 +1,27 @@
+public class fff {
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number == 2) {
+            return true;
+        }
+        if (number % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= number; i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int number = 3;
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
